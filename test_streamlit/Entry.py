bc486b4490d2df1d3c95fcd133a3d1fe1d9f0b1c
CodeStalker <56925232+Floo-zy@users.noreply.github.com>
2024-04-29 20:11:27 +0530

    Added files

---
+++ test_streamlit/Entry.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+    layout="wide"
+)
+
+st.write("# Capstone ! 👋")
+
+st.markdown(
+    """
+    # DEMO
+    """
+)
+
